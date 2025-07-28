d65654f5ee916fcff39a1a661e6bd930433fb16b
Dominik Nakamura <dnaka91@gmail.com>
2025-07-28 17:36:25 +0200

    refactor: use divan for benchmarking

--- benches/parse.rs
+++ benches/parse.rs
@@ -1,32 +1,26 @@
 use std::hint::black_box;
 
-use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
-
-pub fn parse(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Parse");
-
-    for i in ["1.2.3", "1.2.3-pre", "1.2.3+abc", "1.2.3-pre+abc"].iter() {
-        group.bench_with_input(BenchmarkId::new("SemVer", i), i, |b, i| {
-            b.iter(|| semver::Version::parse(black_box(i)))
-        });
-    }
-
-    for i in [
-        "2019.01.06",
-        "2019.01.06.1",
-        "2019.01.06-test",
-        "2019.01.06.1-test",
-        "2019.01.06.1-test.1",
-    ]
-    .iter()
-    {
-        group.bench_with_input(BenchmarkId::new("ChronVer", i), i, |b, i| {
-            b.iter(|| chronver::Version::parse(black_box(i)))
-        });
-    }
-
-    group.finish();
+fn main() {
+    divan::main();
 }
 
-criterion_group!(benches, parse);
-criterion_main!(benches);
+#[divan::bench(args = [
+    "1.2.3",
+    "1.2.3-pre",
+    "1.2.3+abc",
+    "1.2.3-pre+abc",
+])]
+fn semver(value: &str) -> semver::Version {
+    semver::Version::parse(black_box(value)).unwrap()
+}
+
+#[divan::bench(args = [
+    "2019.01.06",
+    "2019.01.06.1",
+    "2019.01.06-test",
+    "2019.01.06.1-test",
+    "2019.01.06.1-test.1",
+])]
+fn chronver(value: &str) -> chronver::Version {
+    chronver::Version::parse(black_box(value)).unwrap()
+}
